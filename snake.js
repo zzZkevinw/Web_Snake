afaed95b5c54f78092b28224c339cecd03888cbc
Kaiwen Zhu <1522906545@qq.com>
2023-08-09 15:37:36 +0800

    v1.0
    v1.0

--- snake.js
+++ snake.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 const BLOCK_SIZE = 20; // 这里保持 BLOCK_SIZE 为常量
-let SNAKE_SPEED = 150;
+let SNAKE_SPEED = 250;
 let snakeX = canvas.width / 2 - BLOCK_SIZE / 2;
 let snakeY = canvas.height / 2 - BLOCK_SIZE / 2;
 let snakeXChange = BLOCK_SIZE;
@@ -11,6 +11,7 @@ let foodX;
 let foodY;
 let score = 0;
 let gameLoop;
+let changeDirection = false; // 用于跟踪方向是否已经改变
 
 function setBoardSize(size) {
     canvas.width = size;
@@ -40,8 +41,9 @@ function generateFood() {
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "green";
+    ctx.fillStyle = "#0D0008"; // 设置深紫色背景
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
     snakeBody.forEach(block => {
         ctx.fillRect(block.x, block.y, BLOCK_SIZE, BLOCK_SIZE);
     });
@@ -49,8 +51,8 @@ function draw() {
     ctx.fillRect(foodX, foodY, BLOCK_SIZE, BLOCK_SIZE);
     ctx.fillStyle = "black";
     ctx.font = "15px Arial";
-    ctx.fillText("Score: " + score, 10, 30);
 }
+
 
 function drawGrid() {
     // ctx.strokeStyle = "#ccc";
@@ -73,12 +75,16 @@ function drawGrid() {
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
 }
 
+function updateScore() {
+    document.getElementById("scoreBoard").innerText = "Score: " + score;
+}
 
 function update() {
     snakeX += snakeXChange;
     snakeY += snakeYChange;
     if (snakeX === foodX && snakeY === foodY) {
         score += 10;
+        updateScore(); // 更新分数
         generateFood();
     } else {
         snakeBody.pop();
@@ -92,6 +98,7 @@ function update() {
     }
     draw();
     drawGrid();
+    changeDirection = false; // 重置方向改变标记
 }
 
 function checkCollision() {
@@ -104,24 +111,29 @@ function checkCollision() {
 }
 
 document.addEventListener("keydown", (event) => {
+    if (changeDirection) return; // 如果方向已经改变，则忽略此次按键事件
+
     if (event.key === "ArrowLeft" && snakeXChange !== BLOCK_SIZE) snakeXChange = -BLOCK_SIZE, snakeYChange = 0;
     if (event.key === "ArrowRight" && snakeXChange !== -BLOCK_SIZE) snakeXChange = BLOCK_SIZE, snakeYChange = 0;
     if (event.key === "ArrowUp" && snakeYChange !== BLOCK_SIZE) snakeYChange = -BLOCK_SIZE, snakeXChange = 0;
     if (event.key === "ArrowDown" && snakeYChange !== -BLOCK_SIZE) snakeYChange = BLOCK_SIZE, snakeXChange = 0;
+
+    changeDirection = true; // 标记方向已经改变
 });
+
 
 function startGame() {
     if (gameLoop) {
         clearInterval(gameLoop);
     }
     // 重置蛇的位置
-    // 设置蛇的起始位置为网格的中心
     snakeX = Math.floor(canvas.width / 2 / BLOCK_SIZE) * BLOCK_SIZE - 20;
     snakeY = Math.floor(canvas.height / 2 / BLOCK_SIZE) * BLOCK_SIZE - 20;
     snakeXChange = BLOCK_SIZE;
     snakeYChange = 0;
     snakeBody = [];
-    score = 0;
+    score = 0; // 重置分数
+    updateScore(); // 更新分数显示
     generateFood();
     gameLoop = setInterval(update, SNAKE_SPEED);
     drawGrid();
@@ -140,6 +152,21 @@ document.getElementsByName('size').forEach(radio => {
     });
 });
 
+window.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+        event.preventDefault(); // 阻止默认的滚动行为
+    }
+});
+
 document.getElementById('startBtn').addEventListener('click', startGame);
+
+document.getElementById('instructionsButton').addEventListener('click', function() {
+    var instructions = document.getElementById('instructions');
+    if (instructions.style.display === 'none') {
+        instructions.style.display = 'block';
+    } else {
+        instructions.style.display = 'none';
+    }
+});
 
 drawGrid();
